fcb35533f7da7f0876c4fe40e757c08ffb0c2a9a
Koo Min Jun <jooon0711@kookmin.ac.kr>
2016-11-23 01:48:56 +0900

    Term Project, 11/23 : countUp, countDown 레이아웃 생성

--- TermProject/src/main/java/my/kmucs/com/koo_timer/Fragment1.java
+++ TermProject/src/main/java/my/kmucs/com/koo_timer/Fragment1.java
@@ -1,21 +1,36 @@
 package my.kmucs.com.koo_timer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Koo on 2016-11-21.
  */
 
 public class Fragment1 extends Fragment {
+    Intent i;
+    Button countUp;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+        countUp = (Button)rootView.findViewById(R.id.countUp);
+        i = new Intent(getActivity(), CountUpActivity.class);
+
+        countUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
 
         return rootView;
     }
--- TermProject/src/main/java/my/kmucs/com/koo_timer/Fragment2.java
+++ TermProject/src/main/java/my/kmucs/com/koo_timer/Fragment2.java
@@ -1,22 +1,38 @@
 package my.kmucs.com.koo_timer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Koo on 2016-11-21.
  */
 
 public class Fragment2 extends Fragment {
+    Intent i;
+    Button countDown;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment2, container, false);
+
+
+        countDown = (Button)rootView.findViewById(R.id.countDown);
+        i = new Intent(getActivity(), CountDownActivity.class);
+
+
+        countDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
 
         return rootView;
     }
